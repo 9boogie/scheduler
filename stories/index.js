9c9b41bd40b5580408790c85c0ee4495609c01b9
9boogie <jzero.youngs89@gmail.com>
2020-09-29 22:15:14 -0500

    Add Appointment components

--- stories/index.js
+++ stories/index.js
@@ -10,11 +10,13 @@ import DayListItem from "components/DayListItem.js";
 import DayList from "components/DayList.js";
 import InterviewerListItem from "components/InterviewerListItem.js";
 import InterviewerList from "components/InterviewerList.js";
-import Appointment from "components/Appointment/index.js"
-import Header from "components/Appointment/Header.js"
-import Empty from "components/Appointment/Empty.js"
-import Show from "components/Appointment/Show.js"
-import Confirm from "components/Appointment/Confirm.js"
+import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show.js";
+import Confirm from "components/Appointment/Confirm.js";
+import Status from "components/Appointment/Status.js";
+import Error from "components/Appointment/Error.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -146,4 +148,10 @@ storiesOf("Appointment", module)
     message='Delete the appointment?'
     onConfirm={action("onConfirm")}
     onCancel={action("onCancel")} 
+  />))
+.add("Status", () => <Status message="Deleting" />)
+.add("Error", () => (
+  <Error 
+    message="Could not delete appointment."
+    onClose={action("onClose")} 
   />));
